93ea1cf12dce8c1c5005fdb6042eee3c7acf7a58
parmardhavalec <dhaval@Administrato.sevenpv.com>
2016-06-24 12:01:15 +0530

    Creation of Project
    created a project and commit

---
+++ ShopList/ShopList/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ShopList
+//
+//  Created by Dhaval on 24/06/16.
+//  Copyright © 2016 Dhaval. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
